0aee4c9436767e512b1ba1a319fdce45e0eba7f6
Chris DiMartino <christopher.dimartino@nytimes.com>
2014-02-05 13:07:37 -0500

    Adding community java cookbook

--- elasticsearch/metadata.rb
+++ elasticsearch/metadata.rb
@@ -6,4 +6,4 @@ description      "Installs elasticsearch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
 version          "0.1.0"
 
-depends "opsworks_java"
+depends "java"
