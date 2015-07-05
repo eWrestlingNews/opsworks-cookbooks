0e4025d2a8d484c94b5c27931267d045842a735a
Chris DiMartino <vwchris@gmail.com>
2015-07-05 11:31:12 -0400

    Working varnish default.vcl now

--- varnish/recipes/server.rb
+++ varnish/recipes/server.rb
@@ -1,6 +1,6 @@
 service "varnish" do
   supports :status => true, :restart => true, :reload => true
-  action [ :enable, :start ]
+  action :nothing
 end
 
 template "/etc/varnish/default.vcl" do
@@ -9,4 +9,8 @@ template "/etc/varnish/default.vcl" do
   group "root"
   mode 0660
   notifies :reload, 'service[varnish]', :immediately
+end
+
+service "varnish" do
+  action [ :enable, :start ]
 end
